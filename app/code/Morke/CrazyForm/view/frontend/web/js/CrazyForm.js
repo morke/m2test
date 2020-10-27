7086339f9a0163aeb751fecc333022ff2c612396
Morke <contact@morke.me>
2020-10-27 12:52:39 +0200

    do some shit. revert to here

---
+++ app/code/Morke/CrazyForm/view/frontend/web/js/CrazyForm.js
@@ -0,0 +1,16 @@
+define(['jquery', 'uiComponent', 'ko', 'Magento_Ui/js/form/form' ], function ($, Component, ko) {
+        'use strict';
+        return Component.extend({
+  /*          defaults: {
+                template: '',
+
+            },*/
+
+
+            initialize: function () {
+                console.log(this);
+                this._super();
+            }
+        });
+    }
+);
