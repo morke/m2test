ba4d99ae9764d2f378152bfb34f22f325c2b8858
Morke <contact@morke.me>
2020-10-28 13:31:34 +0200

    some part of select

--- app/code/Morke/CrazyForm/view/frontend/web/js/CrazyForm.js
+++ app/code/Morke/CrazyForm/view/frontend/web/js/CrazyForm.js
@@ -1,16 +1,27 @@
-define(['jquery', 'uiComponent', 'ko', 'Magento_Ui/js/form/form' ], function ($, Component, ko) {
+define(['jquery', 'uiComponent', 'ko' ], function ($, Component, ko) {
         'use strict';
         return Component.extend({
-  /*          defaults: {
-                template: '',
-
-            },*/
-
 
             initialize: function () {
-                console.log(this);
                 this._super();
-            }
+                //this.getCountry();
+                console.log(this);
+            },
+
+
+            getCountry : function () {
+                $.get('//restcountries.eu/rest/v2/all', function(data) {
+                    $.each(data, function(index, val) {
+                        console.log(val.name)
+                    });
+                })
+            },
+
+            onSubmit: function () {
+                alert("BLYAAAAT");
+            },
+
+
         });
     }
 );
